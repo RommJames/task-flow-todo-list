e933f2a24094a653a789c0b18d789413c12bc6d8
RommJames <rj.cuya010@gmail.com>
2024-11-18 07:57:45 +0800

    checkbox styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 // Main Index
 
 import "./style.css";
+
+// Templates
+// {
+//     <div class="cards-container">
+//             <div class="card">
+//                 <h2 class="card-title">
+//                     <label class="checkbox-container">
+//                         <input type="checkbox">
+//                         <span class="checkmark"></span>
+//                     </label>
+//                     Project Name
+//                 </h2>
+//                 <p class="card-description"> 
+//                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores quidem ad voluptates reprehenderit ratione voluptatum quam aut odit! Sit facere itaque facilis harum sunt unde repellat laborum, impedit enim ut?
+//                 </p>
+//                 <div class="task-properties">
+//                     <div id="due-date">
+//                         Due Date:
+//                         <span id="due-date-value" class="low">MM/DD/YYYY</span>
+//                     </div>
+//                     <div id="priority-level">
+//                         Priority:
+//                         <span id="priority-level-value" class="low">Low</span>
+//                     </div>
+//                 </div>
+//                 <div class="card-btns">
+//                     <div class="btn btn-view">
+//                         Manage Task
+//                     </div>
+//                     <div class="btn btn-delete">
+//                         Delete Project
+//                     </div>
+//                 </div>
+//             </div>               
+//         </div>        
+// }
