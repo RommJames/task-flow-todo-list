27bacffdadea5d1be9a890977ea85b600cfc7943
RommJames <rj.cuya010@gmail.com>
2024-11-18 10:46:12 +0800

    UI Design for Manage Project Task

--- src/index.js
+++ src/index.js
@@ -5,35 +5,118 @@ import "./style.css";
 // Templates
 
 // Projects
-//     <div class="cards-container">
-//             <div class="card">
-//                 <h2 class="card-title">
-                    // <label class="checkbox-container">
-                    //     <input type="checkbox">
-                    //     <span class="checkmark"></span>
-                    // </label>
-//                     Project Name
-//                 </h2>
-//                 <p class="card-description"> 
-//                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores quidem ad voluptates reprehenderit ratione voluptatum quam aut odit! Sit facere itaque facilis harum sunt unde repellat laborum, impedit enim ut?
-//                 </p>
-//                 <div class="task-properties">
-//                     <div id="due-date">
-//                         Due Date:
-//                         <span id="due-date-value" class="low">MM/DD/YYYY</span>
-//                     </div>
-//                     <div id="priority-level">
-//                         Priority:
-//                         <span id="priority-level-value" class="low">Low</span>
-//                     </div>
-//                 </div>
-//                 <div class="card-btns">
-//                     <div class="btn btn-view">
-//                         Manage Task
-//                     </div>
-//                     <div class="btn btn-delete">
-//                         Delete Project
-//                     </div>
-//                 </div>
-//             </div>               
-//         </div>        
+{/* <div class="cards-container">
+            <div class="card">
+                <h2 class="card-title">
+                    <label class="checkbox-container">
+                        <input type="checkbox">
+                        <span class="checkmark"></span>
+                    </label>
+                    Project Name
+                </h2>
+                <p class="card-description"> 
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores quidem ad voluptates reprehenderit ratione voluptatum quam aut odit! Sit facere itaque facilis harum sunt unde repellat laborum, impedit enim ut?
+                </p>
+                <div class="task-properties">
+                    <div id="due-date">
+                        Due Date:
+                        <span id="due-date-value" class="low">MM/DD/YYYY</span>
+                    </div>
+                    <div id="priority-level">
+                        Priority:
+                        <span id="priority-level-value" class="low">Low</span>
+                    </div>
+                </div>
+                <div class="card-btns">
+                    <div class="btn btn-view">
+                        Manage Task
+                    </div>
+                    <div class="btn btn-delete">
+                        Delete Project
+                    </div>
+                </div>
+            </div>            
+            <div class="card project-complete">
+                <h2 class="card-title">
+                    <label class="checkbox-container">
+                        <input type="checkbox">
+                        <span class="checkmark"></span>
+                    </label>
+                    Project Name
+                </h2>
+                <p class="card-description"> 
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores quidem ad voluptates reprehenderit ratione voluptatum quam aut odit! Sit facere itaque facilis harum sunt unde repellat laborum, impedit enim ut?
+                </p>
+                <div class="task-properties">
+                    <div id="due-date">
+                        Due Date:
+                        <span id="due-date-value" class="low">MM/DD/YYYY</span>
+                    </div>
+                    <div id="priority-level">
+                        Priority:
+                        <span id="priority-level-value" class="low">Low</span>
+                    </div>
+                </div>
+                <div class="card-btns">
+                    <div class="btn btn-view">
+                        Manage Task
+                    </div>
+                    <div class="btn btn-delete">
+                        Delete Project
+                    </div>
+                </div>
+            </div>               
+        </div>     */}
+
+// Listing Tasks
+{/* <div class="listing-container">
+            <div class="list list-complete">
+                <div class="list-details">
+                    <h3 class="list-title"> 
+                        <label class="checkbox-container">
+                            <input type="checkbox">
+                            <span class="checkmark"></span>
+                        </label>
+                        <span class="list-title-value">list-title</span>
+                    </h3>
+                    <p class="list-description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique aspernatur placeat vitae labore eum inventore dolor. Rem ad sit nisi iusto eos assumenda omnis deleniti, officiis quas illo labore non nostrum voluptatibus consequuntur est nam magnam nihil? Magnam, dolores? Voluptatibus quia minima labore, quisquam sunt consectetur! Eius rem doloribus nam. Praesentium provident numquam beatae hic quo dicta vitae minima quae reiciendis enim placeat assumenda alias labore et corporis pariatur, voluptatum non voluptates! At in aliquid illum facilis labore repudiandae nihil commodi modi, totam soluta nemo nesciunt, veritatis molestiae excepturi nostrum et maiores non ex sint voluptas nam provident deserunt cumque.</p>
+                </div>          
+                <div class="list-due-date">
+                        Due Date:
+                        <span id="manage-task-due-date-value" class="low due-date-value">MM/DD/YYYY</span>
+                    </div>      
+                <div class="list-btns">
+                    <div class="btn-list-edit">
+                        <img src="media/icons/note-edit-outline.svg" alt="edit">
+                    </div>
+                    <div class="btn-list-edit">
+                        <img src="media/icons/delete-circle-outline.svg" alt="delete">
+                    </div>
+                </div>
+            </div>
+
+            <div class="list">
+                <div class="list-details">
+                    <h3 class="list-title"> 
+                        <label class="checkbox-container">
+                            <input type="checkbox">
+                            <span class="checkmark"></span>
+                        </label>
+                        <span class="list-title-value">list-title</span>
+                    </h3>
+                    <p class="list-description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique aspernatur placeat vitae labore eum inventore dolor. Rem ad sit nisi iusto eos assumenda omnis deleniti, officiis quas illo labore non nostrum voluptatibus consequuntur est nam magnam nihil? Magnam, dolores? Voluptatibus quia minima labore, quisquam sunt consectetur! Eius rem doloribus nam. Praesentium provident numquam beatae hic quo dicta vitae minima quae reiciendis enim placeat assumenda alias labore et corporis pariatur, voluptatum non voluptates! At in aliquid illum facilis labore repudiandae nihil commodi modi, totam soluta nemo nesciunt, veritatis molestiae excepturi nostrum et maiores non ex sint voluptas nam provident deserunt cumque.</p>
+                </div>                
+                <div class="list-due-date">
+                        Due Date:
+                        <span id="manage-task-due-date-value" class="low due-date-value">MM/DD/YYYY</span>
+                    </div>
+                <div class="list-btns">
+                    <div class="btn-list-edit">
+                        <img src="media/icons/note-edit-outline.svg" alt="edit">
+                    </div>
+                    <div class="btn-list-edit">
+                        <img src="media/icons/delete-circle-outline.svg" alt="delete">
+                    </div>
+                </div>
+            </div>
+        </div> */}
